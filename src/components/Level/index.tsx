81099623b6506f76350759ae4132592141a528d1
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-08-07 16:00:48 -0300

    transição de animação

--- src/components/Level/index.tsx
+++ src/components/Level/index.tsx
@@ -9,6 +9,7 @@ import {
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
+  withTiming,
 } from "react-native-reanimated";
 
 import { THEME } from "../../styles/theme";
@@ -41,11 +42,11 @@ export function Level({
   const COLOR = TYPE_COLORS[type];
 
   function onPressIn() {
-    scale.value = 1.1;
+    scale.value = withTiming(1.1);
   }
 
   function onPressOut() {
-    scale.value = 1;
+    scale.value = withTiming(1);
   }
 
   return (
